f0340a24e594e13888d9d6f9b4652ce6f91fc097
akbelouafa <abdelkrimbelouafa@gmail.com>
2020-04-13 17:36:25 +0200

    init TODO API project

---
+++ src/main/java/com/ak/rest/todo/aktodoservice/AkTodoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ak.rest.todo.aktodoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkTodoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkTodoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ak/rest/todo/aktodoservice/AkTodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak.rest.todo.aktodoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkTodoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
